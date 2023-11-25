992824f6d64f3830939999c4cfdccd236d3acfb7
jorenchik <jorens.stekels@gmail.com>
2023-11-25 09:55:27 +0200

    sandbox: refactor numpy game state

--- snakext/sandbox/np-test.py
+++ snakext/sandbox/np-test.py
@@ -14,24 +14,32 @@ REMOTE_SNAKE_TAIL = "t2"
 WALL = "w"
 FOOD = "f"
 POISON = "p"
+VOID = "v"
 
 constants = [
     LOCAL_SNAKE_HEAD, LOCAL_SNAKE_BODY, LOCAL_SNAKE_TAIL, REMOTE_SNAKE_HEAD,
     REMOTE_SNAKE_BODY, REMOTE_SNAKE_TAIL, WALL, FOOD, POISON
 ]
 
-resulting_sums = []
-duplicates = []
-for i in range(0, len(constants) - 1):
-    for k in range(i, len(constants) - 1):
-        sum = constants[i] + constants[k + 1]
-        print(constants[i], constants[k + 1], sum, sep=' and ')
-        if sum not in resulting_sums:
-            resulting_sums.append(sum)
-        else:
-            duplicates.append(sum)
 
-print(duplicates)
+def check_sum_duplicates(strings: list[str]) -> bool:
+    resulting_sums = []
+    duplicates = []
+    for i in range(0, len(constants) - 1):
+        for k in range(i, len(constants) - 1):
+            sum = constants[i] + constants[k + 1]
+            print(constants[i], constants[k + 1], sum, sep=' and ')
+            if sum not in resulting_sums:
+                resulting_sums.append(sum)
+            else:
+                duplicates.append(sum)
+    return len(duplicates) > 0
 
-arr = np.array([x for x in range(0, column_count * row_count)
+
+arr = np.array([VOID for x in range(0, column_count * row_count)
                 ]).reshape(row_count, column_count)
+
+arr_2 = np.array([VOID for x in range(0, column_count * row_count)
+                  ]).reshape(row_count, column_count)
+
+print(np.char.add(arr, arr_2))
