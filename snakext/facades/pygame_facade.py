52115ac4a24d57425e37d610985f3be07ec4bca5
jorenchik <jorens.stekels@gmail.com>
2024-01-07 10:15:44 +0200

    feat: pygame facade docstrings

--- snakext/facades/pygame_facade.py
+++ snakext/facades/pygame_facade.py
@@ -1,7 +1,12 @@
-""" Offers simpler interface for pygame functionality. """
+"""
+This module provides a simplified interface for Pygame functionality, 
+offering easy-to-use functions for game development tasks such as 
+initializing the game window, handling input, and rendering graphics.
+Designed to streamline common Pygame operations, it's particularly 
+useful for rapid prototyping and educational purposes.
+"""
 import pygame
 import sys
-import itertools
 from types import ModuleType
 from typing import Callable
 from snakext.game.state import state
@@ -43,10 +48,22 @@ EXIT_KEYS = (pygame.K_q, )
 
 
 def get_ticks_seconds() -> float:
+    """
+    Returns the number of seconds since the Pygame library was initialized.
+    This is achieved by dividing the number of milliseconds from `pygame.time.get_ticks()`
+    by 1000 to convert it to seconds.
+    """
     return pygame.time.get_ticks() / 1000
 
 
 def init_game(pygame_module: ModuleType = pygame) -> None:
+    """
+    Initializes the game environment using the Pygame library.
+    This function sets up the main screen and game clock.
+
+    Args:
+        pygame_module (ModuleType, optional): The Pygame module to be used. Defaults to pygame.
+    """
     global _screen, _clock, _pygame_module
     pygame_module.init()
     _pygame_module = pygame_module
@@ -58,6 +75,10 @@ def init_game(pygame_module: ModuleType = pygame) -> None:
 
 
 def show_screen() -> None:
+    """
+    Displays the game screen with the default resolution.
+    This function re-initializes the display mode of the screen.
+    """
     global _screen
     _screen = pygame.display.set_mode(DEFAULT_RESOLUTION, )
 
@@ -70,6 +91,18 @@ error = pygame.error
 def movement_key(previous_keys: list[int],
                  get_movement_keys: Callable[[], list[int]],
                  default_movement_key: int) -> int:
+    """
+    Determines the new movement key based on the previous keys and the current movement keys.
+    If no new movement key is detected, returns the default movement key.
+
+    Args:
+        previous_keys (list[int]): List of keys previously pressed.
+        get_movement_keys (Callable[[], list[int]]): Function to get the current movement keys.
+        default_movement_key (int): The default movement key to return if no new key is found.
+
+    Returns:
+        int: The new movement key or the default movement key.
+    """
     new_movement_key = movement_direction(
         previous_keys,
         get_movement_keys,
@@ -83,6 +116,17 @@ def movement_direction(
     get_keys: Callable[[], list[int]],
     default_direction: int,
 ) -> int:
+    """
+    Calculates the new movement direction based on the pressed keys.
+
+    Args:
+        previous_movement_keys (list[int]): List of previously pressed movement keys.
+        get_keys (Callable[[], list[int]]): Function to obtain the currently pressed keys.
+        default_direction (int): Default direction to fall back to if no new direction is detected.
+
+    Returns:
+        int: The new movement direction key, or the default direction if no change is detected.
+    """
     pressed_keys = get_keys()
     # Detect newly pressed key that would change direction
     new_pressed_keys = [
@@ -95,6 +139,16 @@ def movement_direction(
 
 
 def tick(fps: int = 0) -> float:
+    """
+    Advances the game clock by one tick and returns the time spent per frame in seconds.
+    Can also be used to control the frame rate of the game.
+
+    Args:
+        fps (int, optional): The desired frame rate (frames per second). Defaults to 0 (unlimited).
+
+    Returns:
+        float: The time spent per frame in seconds.
+    """
     global _clock
     return _clock.tick(fps) / 1000
 
